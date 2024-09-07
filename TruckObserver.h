1623b90bc35498b480dcaf06ef88f00eb764bd08
Henru Matthis <u23526158@tuks.co.za>
2024-09-07 15:57:18 +0200

    Add files via upload

--- TruckObserver.h
+++ TruckObserver.h
@@ -2,7 +2,11 @@
 #ifndef PRAC_4_TRUCKOBSERVER_H
 #define PRAC_4_TRUCKOBSERVER_H
 
-#include "CropField.h"
+#include <vector>
+class CropField;
+//#include "TruckLogisticsManager.h"
+//#include "FertilizerDecorator.h"
+//#include "ExtraBarnDecorator.h"
 class TruckObserver {
 public:
     void update(CropField* field);
--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
-//
-// Created by Rhulani Matiane on 2024/09/02.
 #include <iostream>
+#include "Barn.h"
+#include "BreadthFirstIterator.h"
+#include "CropField.h"
+#include "CropFieldDecorator.h"
+#include "DeliveryTruck.h"
+#include "DephtFirstIterator.h"
+#include "DrySoil.h"
+#include "ExtraBarnDecorator.h"
+#include "FarmUnit.h"
+#include "FarmTraversalContext.h"
+#include "FertilizerTruck.h"
+#include "FertilizerDecorator.h"
+#include "FloodedSoil.h"
+#include "FruitfulSoil.h"
+#include "Iterator.h"
+#include "SoilState.h"
+#include "TruckObserver.h"
+#include "TraversalContext.h"
+#include "TruckLogisticsManager.h"
 
-int main(){
-    std::cout << "Hello world" << std::endl;
+int main()
+{
+    std::cout<<"Hello World";
+
+    return 0;
 }
